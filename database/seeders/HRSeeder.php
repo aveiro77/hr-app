f5cc23ff0f9dd69cee76084daff19807572676fd
aveiro77 <aveiro.seven88@gmail.com>
2025-05-07 22:19:21 +0700

    HRSeeder

--- database/seeders/HRSeeder.php
+++ database/seeders/HRSeeder.php
@@ -15,6 +15,77 @@ class HRSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $faker=Faker::create();
+
+        DB:table('departements')->insert([
+            [
+                'name' => 'HR',
+                'description' => 'Human Resources',
+                'status' => 'active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name' => 'IT',
+                'description' => 'Information Technology',
+                'status' => 'active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Sales',
+                'description' => 'Sales',
+                'status' => 'active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name' => 'FA',
+                'description' => 'Finance & Accounting',
+                'status' => 'active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Supply Chain',
+                'description' => 'Supply Chain',
+                'status' => 'active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'name' => 'Purchasing',
+                'description' => 'Purchasing',
+                'status' => 'active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ]
+        ]);
+
+        DB:table('roles')->insert([
+            [
+                'title' => 'HR',
+                'description' => 'Handling team',
+                'status' => 'active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'title' => 'Developer',
+                'description' => 'Handling software',
+                'status' => 'active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+            [
+                'title' => 'User',
+                'description' => 'Handling job',
+                'status' => 'active',
+                'created_at' => Carbon::now(),
+                'updated_at' => Carbon::now()
+            ],
+        ]);
+
+        
     }
 }
